e6ef9f176d20a6fae66baeb04ea002cbfcedb761
MarconZet <25779550+MarconZet@users.noreply.github.com>
2023-01-07 12:48:50 +0100

    betrer oparibility

--- update-kconf.sh
+++ update-kconf.sh
@@ -1,10 +1,14 @@
-#!/bin/zsh
+#!/bin/sh
 
 aws eks update-kubeconfig --name "$(terraform output --raw cluster_name)"
+
+# test cluster
 helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 helm install nginx-ingress ingress-nginx/ingress-nginx \
   --create-namespace --namespace ingress \
   --set controller.replicaCount=2
-k apply -f https://raw.githubusercontent.com/volcano-sh/volcano/master/installer/volcano-development.yaml
-k apply -f queue.yml
-k apply -f job1.yml
+
+# crate volcano
+kubectl apply -f https://raw.githubusercontent.com/volcano-sh/volcano/master/installer/volcano-development.yaml
+kubectl apply -f queue.yml
+kubectl apply -f job1.yml
